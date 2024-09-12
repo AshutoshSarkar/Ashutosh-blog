e7f6d9f2be34e4f01469d4baa10e82bba3db6ff6
Ashutosh Sarkar <ashut.sarkar8187@gmail.com>
2024-09-12 12:59:33 +0530

    added functionalities for production

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
-        target:'http://localhost:3000',
-        secure:false, // 这里是你的后
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false, // Disable SSL verification if your backend is using HTTP
       },
     },
   },
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Specify the output directory explicitly
+  },
 });
